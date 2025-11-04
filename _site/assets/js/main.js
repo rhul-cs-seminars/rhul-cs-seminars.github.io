75f6459ae1c3808a5d3fc9dc57897b6452c007e7
Anand <anandtrex@users.noreply.github.com>
2025-11-04 23:35:04 +0000

    Remove _site from github

--- _site/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleText() {
-  var x = document.getElementById("show-info");
-  if (x.innerHTML === "Show Info") {
-    x.innerHTML = "Hide Info";
-  } else {
-    x.innerHTML = "Show Info";
-  }
-}
